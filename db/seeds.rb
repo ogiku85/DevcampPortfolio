98c14e49dbc6a86b07e586102da3520b81b2e25c
Osamuyimen Ogiku <ogiku85@yahoo.co.uk>
2022-04-25 21:52:34 +0200

    Implemented custom scopes for portfolio items

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,12 +5,25 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+# Seed Topic data
+3.times do |n|
+  Topic.create!(
+    title: "Topic Title #{n + 1}"
+  )
+end
 
+puts "3 topics created."
 # Seed Blogs data
 10.times do |n|
   Blog.create!(
     title: "Blog Title #{n + 1}",
-    body: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
+    body: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
+           tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+           quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
+           Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.
+           Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est
+           laborum.",
+    topic_id: Topic.last.id
   )
 end
 
@@ -24,10 +37,20 @@ puts "10 blog posts created."
   )
 end
 puts "5 skills created."
-9.times do |n|
+8.times do |n|
   Portfolio.create!(
     title: "Portfolio Title #{n + 1}",
-    subtitle:"Portfolio Sub title  #{n + 1}",
+    subtitle:"Ruby On Rails",
+    body: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+    main_image:"https://via.placeholder.com/600x400",
+    thumb_image:"https://via.placeholder.com/350x200"
+  )
+end
+
+1.times do |n|
+  Portfolio.create!(
+    title: "Portfolio Title #{n + 1}",
+    subtitle:"Angular",
     body: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
     main_image:"https://via.placeholder.com/600x400",
     thumb_image:"https://via.placeholder.com/350x200"
