d9e9c1de9ed8142f54ef58b56bbb82fad2481343
Osamuyimen Ogiku <ogiku85@yahoo.co.uk>
2022-04-30 23:58:18 +0200

    Added new parameters for user account editing and registration

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,18 @@
 class ApplicationController < ActionController::Base
+  # before_filter :configure_permitted_parameters, if: :devise_controller?
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
+    devise_parameter_sanitizer.permit(:account_update, keys: [:name])
+  end
+  # def after_sign_in_path_for(resource)
+  #   if current_user.has_role?(:admin)
+  #     dashboard_path
+  #   elsif current_user.has_role?(:student)
+  #     root_path
+  #   else
+  #     root_path
+  #   end
+  # end
 end
